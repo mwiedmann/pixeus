42b047e6d7997b12eac4bfecf0d5ec8ff5f7eeb1
Mark Wiedmann <mark.wiedmann@unqork.com>
2023-01-30 14:22:13 -0500

    chore: common AISprite function

--- game/gamesprites.c
+++ game/gamesprites.c
@@ -31,6 +31,37 @@ void spriteIRQConfig() {
     x16SpriteCollisionsEnable();
 }
 
+void standardAISpriteConfig(AISprite *sp, EnemyLayout *layout, unsigned char index)
+{
+    sp->sprite.index = index;
+    sp->sprite.spriteBank = 1;
+    sp->sprite.clrMode = 1;
+    
+    // Possible zDepth will change but leave for now
+    sp->sprite.zDepth = BetweenL0L1;
+
+    sp->sprite.graphicsBank = 0;
+    sp->sprite.animationCount = 0;    
+    sp->sprite.animationStopFrame = 0;
+    sp->sprite.animationDirection = 1;
+    sp->sprite.animationFrame = 0;
+    sp->sprite.x = layout->x * TILE_PIXEL_WIDTH;
+    sp->sprite.y = layout->y * TILE_PIXEL_HEIGHT;
+    sp->sprite.xL = sp->sprite.x * MOVE_FACTOR;
+    sp->sprite.yL = sp->sprite.y * MOVE_FACTOR;
+    sp->sprite.lastX = sp->sprite.x;
+    sp->sprite.lastY = sp->sprite.y;
+    
+    sp->xTileStart = layout->x;
+    sp->yTileStart = layout->y;
+    sp->xTileEnd = layout->x + layout->length;
+    sp->yTileEnd = layout->y;
+
+    spriteInit(&sp->sprite);
+    x16SpriteIdxSetXY(sp->sprite.spriteBank, sp->sprite.index, sp->sprite.x, sp->sprite.y);
+    x16SpriteIdxSetHFlip(sp->sprite.spriteBank, sp->sprite.index, sp->sprite.animationDirection);
+}
+
 void playerCreate(Sprite *p, unsigned char index) {
     p->index = index;
     p->spriteBank = 1;
@@ -62,110 +93,44 @@ void playerCreate(Sprite *p, unsigned char index) {
 
 void snakeCreate(AISprite *snake, EnemyLayout *layout, unsigned char index)
 {
-    snake->sprite.index = index;
-    snake->sprite.spriteBank = 1;
-    snake->sprite.clrMode = 1;
     snake->sprite.collisionMask = 0b1011;
-    snake->sprite.zDepth = BetweenL0L1;
     snake->sprite.width = PX16;
     snake->sprite.height = PX16;
-    snake->sprite.graphicsBank = 0;
     snake->sprite.graphicsAddress = SPRITE_MEM_SNAKE;
     snake->sprite.frames = 4;
     snake->sprite.frameSize = 256; // Calculated as width * height
-    snake->sprite.animationCount = 0;
     snake->sprite.animationSpeed = 6;
-    snake->sprite.animationStopFrame = 0;
-    snake->sprite.animationDirection = 1;
-    snake->sprite.animationFrame = 0;
-    snake->sprite.x = layout->x * TILE_PIXEL_WIDTH;
-    snake->sprite.y = layout->y * TILE_PIXEL_HEIGHT;
-    snake->sprite.xL = snake->sprite.x * MOVE_FACTOR;
-    snake->sprite.yL = snake->sprite.y * MOVE_FACTOR;
-    snake->sprite.lastX = snake->sprite.x;
-    snake->sprite.lastY = snake->sprite.y;
     snake->sprite.speed = 6;
 
-    snake->xTileStart = layout->x;
-    snake->yTileStart = layout->y;
-    snake->xTileEnd = layout->x + layout->length;
-    snake->yTileEnd = layout->y;
-
-    spriteInit(&snake->sprite);
-    x16SpriteIdxSetXY(snake->sprite.spriteBank, snake->sprite.index, snake->sprite.x, snake->sprite.y);
-    x16SpriteIdxSetHFlip(snake->sprite.spriteBank, snake->sprite.index, snake->sprite.animationDirection);
+    standardAISpriteConfig(snake, layout, index);
 }
 
 void beeCreate(AISprite *bee, EnemyLayout *layout, unsigned char index)
 {
-    bee->sprite.index = index;
-    bee->sprite.spriteBank = 1;
-    bee->sprite.clrMode = 1;
     bee->sprite.collisionMask = 0b1011;
-    bee->sprite.zDepth = BetweenL0L1;
     bee->sprite.width = PX16;
     bee->sprite.height = PX16;
-    bee->sprite.graphicsBank = 0;
     bee->sprite.graphicsAddress = SPRITE_MEM_BEE;
     bee->sprite.frames = 4;
     bee->sprite.frameSize = 256; // Calculated as width * height
-    bee->sprite.animationCount = 0;
     bee->sprite.animationSpeed = 6;
-    bee->sprite.animationStopFrame = 0;
-    bee->sprite.animationDirection = 1;
-    bee->sprite.animationFrame = 0;
-    bee->sprite.x = layout->x * TILE_PIXEL_WIDTH;
-    bee->sprite.y = layout->y * TILE_PIXEL_HEIGHT;
-    bee->sprite.xL = bee->sprite.x * MOVE_FACTOR;
-    bee->sprite.yL = bee->sprite.y * MOVE_FACTOR;
-    bee->sprite.lastX = bee->sprite.x;
-    bee->sprite.lastY = bee->sprite.y;
     bee->sprite.speed = 13;
 
-    bee->xTileStart = layout->x;
-    bee->yTileStart = layout->y;
-    bee->xTileEnd = layout->x + layout->length;
-    bee->yTileEnd = layout->y;
-
-    spriteInit(&bee->sprite);
-    x16SpriteIdxSetXY(bee->sprite.spriteBank, bee->sprite.index, bee->sprite.x, bee->sprite.y);
-    x16SpriteIdxSetHFlip(bee->sprite.spriteBank, bee->sprite.index, bee->sprite.animationDirection);
+    standardAISpriteConfig(bee, layout, index);
 }
 
 void ghostCreate(AISprite *ghost, EnemyLayout *layout, unsigned char index)
 {
-    ghost->sprite.index = index;
-    ghost->sprite.spriteBank = 1;
-    ghost->sprite.clrMode = 1;
     ghost->sprite.collisionMask = 0b1011;
-    ghost->sprite.zDepth = BetweenL0L1;
     ghost->sprite.width = PX16;
     ghost->sprite.height = PX16;
-    ghost->sprite.graphicsBank = 0;
     ghost->sprite.graphicsAddress = SPRITE_MEM_GHOST;
     ghost->sprite.frames = 4;
     ghost->sprite.frameSize = 256; // Calculated as width * height
-    ghost->sprite.animationCount = 0;
     ghost->sprite.animationSpeed = 20;
-    ghost->sprite.animationStopFrame = 0;
-    ghost->sprite.animationDirection = 1;
-    ghost->sprite.animationFrame = 0;
-    ghost->sprite.x = layout->x * TILE_PIXEL_WIDTH;
-    ghost->sprite.y = layout->y * TILE_PIXEL_HEIGHT;
-    ghost->sprite.xL = ghost->sprite.x * MOVE_FACTOR;
-    ghost->sprite.yL = ghost->sprite.y * MOVE_FACTOR;
-    ghost->sprite.lastX = ghost->sprite.x;
-    ghost->sprite.lastY = ghost->sprite.y;
     ghost->sprite.speed = 3;
 
-    ghost->xTileStart = layout->x;
-    ghost->yTileStart = layout->y;
-    ghost->xTileEnd = layout->x + layout->length;
-    ghost->yTileEnd = layout->y;
-
-    spriteInit(&ghost->sprite);
-    x16SpriteIdxSetXY(ghost->sprite.spriteBank, ghost->sprite.index, ghost->sprite.x, ghost->sprite.y);
-    x16SpriteIdxSetHFlip(ghost->sprite.spriteBank, ghost->sprite.index, ghost->sprite.animationDirection);
+    standardAISpriteConfig(ghost, layout, index);
 }
 
 void bulletCreate(Sprite *b, unsigned char index) {
