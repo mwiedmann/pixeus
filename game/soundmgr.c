343594d665992e4d11a04c80ca4cd5ecd9361d99
Mark Wiedmann <mark.wiedmann@unqork.com>
2023-02-18 23:00:26 -0500

    feat: more levels, sound control

--- game/soundmgr.c
+++ game/soundmgr.c
@@ -6,50 +6,68 @@
 #include "pcmplayer.h"
 #include "zsmplayer.h"
 
+unsigned char MUSIC_ON = 0;
+unsigned char SOUND_ON = 1;
+
 void loadSounds() {
-    pcm_init();
-    zsm_init();
+    if (SOUND_ON) {
+        pcm_init();
+        POKE(0, SOUND_BANK_LASER);
+        cbm_k_setnam("sounds/guylaser.zcm");
+        cbm_k_setlfs(0, 8, 2);
+        cbm_k_load(0, (unsigned short)BANK_RAM);
 
-    POKE(0, SOUND_BANK_LASER);
-    cbm_k_setnam("sounds/guylaser.zcm");
-	cbm_k_setlfs(0, 8, 2);
-    cbm_k_load(0, (unsigned short)BANK_RAM);
+        POKE(0, SOUND_BANK_ALIEN_HIT);
+        cbm_k_setnam("sounds/alienhit.zcm");
+        cbm_k_setlfs(0, 8, 2);
+        cbm_k_load(0, (unsigned short)BANK_RAM);
+    }
 
-    POKE(0, SOUND_BANK_ALIEN_HIT);
-    cbm_k_setnam("sounds/alienhit.zcm");
-	cbm_k_setlfs(0, 8, 2);
-    cbm_k_load(0, (unsigned short)BANK_RAM);
+    if (MUSIC_ON) {
+        zsm_init();
+        POKE(0, MUSIC_BANK);
+        // cbm_k_setnam("sounds/drake.zsm");
+        cbm_k_setnam("sounds/outrun.zsm");
+        cbm_k_setlfs(0, 8, 2);
+        cbm_k_load(0, (unsigned short)BANK_RAM);
 
-    POKE(0, MUSIC_BANK);
-    // cbm_k_setnam("sounds/drake.zsm");
-    cbm_k_setnam("sounds/outrun.zsm");
-	cbm_k_setlfs(0, 8, 2);
-    cbm_k_load(0, (unsigned short)BANK_RAM);
-
-    zsm_startmusic(MUSIC_BANK, (unsigned short)BANK_RAM);
-	zsm_forceloop(0);
+        zsm_startmusic(MUSIC_BANK, (unsigned short)BANK_RAM);
+        zsm_forceloop(0);
+    }
 
     // Back to the default bank
     POKE(0, LEVEL_BANK);
 }
 
 void playSoundsThisGameLoop() {
-    pcm_play();
-    zsm_play();
+    if (SOUND_ON) {
+        pcm_play();
+    }
+
+    if (MUSIC_ON) {
+        zsm_play();
+    }
+
     POKE(0, LEVEL_BANK);
 }
 
 void soundsCleanup() {
-    zsm_stopmusic();
+    if (MUSIC_ON) {
+        zsm_stopmusic();
+    }
 }
 
 void playLaser() {
-    pcm_stop();
-    pcm_trigger_digi(SOUND_BANK_LASER, (unsigned short)BANK_RAM);
+    if (SOUND_ON) {
+        pcm_stop();
+        pcm_trigger_digi(SOUND_BANK_LASER, (unsigned short)BANK_RAM);
+    }
 }
 
 
 void playAlienHit() {
-    pcm_stop();
-    pcm_trigger_digi(SOUND_BANK_ALIEN_HIT, (unsigned short)BANK_RAM);
+    if (SOUND_ON) {
+        pcm_stop();
+        pcm_trigger_digi(SOUND_BANK_ALIEN_HIT, (unsigned short)BANK_RAM);
+    }
 }
